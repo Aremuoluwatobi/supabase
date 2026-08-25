eb82ddd4db618b2f7d2326d55b3b4a393f1b1afc
AremuTaiwo <aremutaiwotobi@gmail.com>
2026-08-26 00:53:32 +0100

    Stage 2: public route and unverified protected route

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
 from db import supabase
+from fastapi import HTTPException
+from pydantic import BaseModel
 from contextlib import asynccontextmanager
+from fastapi import Header
 
 app = FastAPI()
 
 
-@app.get("/health")
-def check_status():
-    return {"status": "running well"}
+class AuthRequest(BaseModel):
+    email: str
+    password: str
 
 
 @asynccontextmanager
@@ -17,3 +20,51 @@ async def lifespan(app: FastAPI):
     print("server shutting down")
 
 app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/health")
+def check_status():
+    return {"status": "running well"}
+
+
+@app.get("/public/info")
+def public_info():
+    return {"message": "Welcome stranger! This info is public."}
+
+
+@app.post("/auth/signup", status_code=201)
+def create_login(request: AuthRequest):
+    if not request.email or not request.password:
+        raise HTTPException(status_code=400, detail="invalid cridentials")
+
+    result = supabase.auth.sign_up({
+        "email": request.email,
+        "password": request.password
+    })
+    return result
+
+
+@app.post("/auth/login")
+def check_login(request: AuthRequest):
+    if not request.email or not request.password:
+        raise HTTPException(status_code=400, detail="need email and passowrd")
+    try:
+        result = supabase.auth.sign_in_with_password({
+            "email": request.email,
+            "password": request.password
+        })
+        return result
+    except Exception:
+        raise HTTPException(
+            status_code=401, detail="invalid login credentials")
+
+
+@app.get("/protected/profile")
+def protected_profile(authorization: str = Header(None, alias="Authorization")):
+    print(f"Received:[{authorization}]")
+    if not authorization:
+        raise HTTPException(status_code=401, detail="Access token required")
+    if not authorization.startswith("Bearer "):
+        raise HTTPException(status_code=401, detail="Access token required")
+    else:
+        return {"message": "token looks valid, not yet verified"}
